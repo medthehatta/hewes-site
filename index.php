0ccdb8d88119ef88538d458445b956c0e4bb2623
Med <medthehatta@gmail.com>
2013-01-17 17:28:02 -0500

    Put header and footer in index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="hewes1.css"/>
+</head>
+
+<body>
+<?php require("include/header.php"); ?>
+
+
+<?php require("include/footer.php"); ?>
+</body>
+</html>

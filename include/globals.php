4e18546ad2c6d5d9f12dc98a3a6b1f393b8f15cd
Med Mahmoud <ahmed@medhatter.com>
2013-01-17 17:52:00 -0500

    Removed debug echos and added a default class for globals.php

--- include/globals.php
+++ include/globals.php
@@ -1,25 +1,21 @@
 <?php
 require_once("functions.php");
 
-echo "Starting session...\n";
 session_start();
 
 // database
-echo "Setting database...\n";
 if (!isset($_SESSION["db"])) { $_SESSION["db"] = defaultdb(); }
 
 // array of classes
-echo "Setting classes...\n";
 if (!isset($_SESSION["classes"])) {
   $_SESSION["classes"] = get_classes($_SESSION["db"]);
 }
 
 // this class, if necessary
-echo "Setting current class...\n";
-if (!isset($_GET["class"]) and isset($_GET["class"])) {
-  $_SESSION["classid"] = $_GET["class"]; 
+if (isset($_GET["class"])) {
+  $_SESSION["classid"] = $_GET["class"];
 }
-  
-echo var_dump($_SESSION);
-
+if (!isset($_SESSION["classid"])) {
+  $_SESSION["classid"] = 1;
+}
 ?>

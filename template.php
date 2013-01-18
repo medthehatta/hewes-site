4be76bc279edeec1555db7befaa0313795e93aab
Med Mahmoud <ahmed@medhatter.com>
2013-01-17 20:02:10 -0500

    Added a template file for Matt

---
+++ template.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<?php require("include/globals.php"); ?>
+<html>
+<head>
+<link rel="stylesheet" href="hewes1.css"/>
+</head>
+
+<body>
+<div id="container">
+
+<div id="header">
+<h1>Home</h1>
+</div>
+<?php require("include/course_menu.php"); ?>
+
+<div id="content">
+<!-- ############################### --!>
+<!-- CONTENT GOES BETWEEN THESE DIVS --!>
+<!-- ############################### --!>
+</div>
+
+<?php require("include/footer.php"); ?>
+</div><!--/container--!>
+</body>
+</html>
